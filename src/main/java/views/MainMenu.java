86a46f05e1910fb45af4c7b937f04b2b567d8776
Jon Bockhorst <jmbockhorst@gmail.com>
2020-04-10 19:20:42 -0500

    Style the main menu

--- src/main/java/views/MainMenu.java
+++ src/main/java/views/MainMenu.java
@@ -2,10 +2,14 @@ package views;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainMenu {
@@ -17,21 +21,38 @@ public class MainMenu {
         VBox gameMenu = new VBox();
 
         Button singlePlayer = new Button("Singleplayer");
+        styleMenuButton(singlePlayer);
         singlePlayer.setOnAction(singlePlayerHandler);
 
         Button multiPlayer = new Button("Multiplayer");
+        styleMenuButton(multiPlayer);
         multiPlayer.setOnAction(multiPlayerHandler);
 
         Button networkGame = new Button("Network game");
+        styleMenuButton(networkGame);
         networkGame.setOnAction(networkGameHandler);
 
         gameMenu.getChildren().add(singlePlayer);
         gameMenu.getChildren().add(multiPlayer);
         gameMenu.getChildren().add(networkGame);
+        gameMenu.setAlignment(Pos.CENTER);
+        gameMenu.setSpacing(20.0);
 
         mainPane.setCenter(gameMenu);
 
+        Label title = new Label("Chess game");
+        title.setFont(Font.font(50.0));
+
+        StackPane pane = new StackPane(title);
+        pane.setAlignment(Pos.CENTER);
+        mainPane.setTop(pane);
+
         stage.setScene(new Scene(mainPane, 600, 600));
+    }
+
+    private static void styleMenuButton(Button button) {
+        button.setFont(Font.font(35));
+        button.setPadding(new Insets(20, 20, 20, 20));
     }
 
 }
